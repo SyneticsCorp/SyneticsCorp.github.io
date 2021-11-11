eb4ddadc1d72d4219c6798e07fb6ba2619d27a27
SyneticsCorp <dongjoon.han@synetics.kr>
2021-11-11 15:34:39 +0900

    템플릿 포스트 업로드

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "초보자를위한 Atlassian Confluence 튜토리얼 : 완전한 가이드",
-        "excerpt":"Atlassian Confluence Tutorial 초보자를위한 Atlassian Confluence 튜토리얼 : 자동차, 철도, 항공, 로봇 등 안전 필수 시스템(Safety Critical System) 들은 사고 발생시 큰 인명 및 재산 피해로 이어지기 때문에 개발 과정에 서 철저한 안전 활동이 요구된다. 특히, 시스템 내 SW의 역할이 중요해지 고 그 비중이 높아짐에 따라, 사고의 잠재적 원인인 해저드(Hazard)를...","categories": ["category2"],
-        "tags": ["tag1","tag2","tag3","tag4","tag5","tag6","tag7"],
-        "url": "/category2/confluence-tutorial-1/",
-        "teaser": ""
-      },{
         "title": "초보자를위한 Atlassian Confluence 튜토리얼 : 2탄",
         "excerpt":"한편, 초지능, 초연결 시대에 진입하면서 머신러닝, 딥러닝 등 AI 기 술이 산업 전반에 활용되고 있으며, 개인지원 로봇 (Personal Care Robot) 의 자율 기능(autonomous function)에도 적용되고 있다. AI 기술은 자율성 을 갖추어 큰 편리함을 제공하지만, 전통적인 알고리즘 기반의 SW가 아 닌 데이터를 기반으로 학습을 통해 개발되는 SW이기 때문에, 기존과는 다른 안전성...","categories": ["category4"],
         "tags": ["tag1","tag2","tag3","tag4","tag5","tag6","tag7"],
@@ -51,5 +45,11 @@ var store = [{
         "excerpt":"소프트웨어 테스팅이란 무엇입니까? 소프트웨어 테스트는 실제 결과가 예상 결과와 일치하는지 확인하고 소프트웨어 시스템에 결함 이 없는지 확인하는 활동 입니다. 하나 이상의 관심 속성을 평가하기위한 소프트웨어 구성 요소 또는 시스템 구성 요소의 실행을 포함합니다. 철도, 항공, 로봇 등 안전 필수 시스템(Safety Critical System) 들은 사고 발생시 큰 인명 및 재산 피해로...","categories": ["category4"],
         "tags": ["tag1","tag2","tag3","tag4","tag5","tag6","tag7"],
         "url": "/category4/SW-Testing/",
+        "teaser": ""
+      },{
+        "title": "포스팅 템플릿",
+        "excerpt":"SW 테스팅 개요 참고 마크다운 작성 예제 ~/docs/_posts 폴더에 여러 템플릿을 참고해주세요. 문단 나누기 -&gt; 글 마지막 에서 스페이스바 2번 + 엔터 or 태그 입력 자동차, 철도, 항공, 로봇 등 안전 필수 시스템(Safety Critical System) 들은 사고 발생시 큰 인명 및 재산 피해로 이어지기 때문에 개발 과정에 서 철저한 안전...","categories": ["category2"],
+        "tags": ["tag1","tag2","tag3","tag4","tag5","tag6","tag7"],
+        "url": "/category2/templates/",
         "teaser": ""
       }]
